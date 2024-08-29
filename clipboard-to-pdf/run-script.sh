04f383073f82928daa1405661bf61af818389b06
Nick Trombley <trombley.nick@gmail.com>
2024-08-28 23:08:52 -0400

    Rename markdown-to-pdf to clipboard-to-pdf

--- clipboard-to-pdf/run-script.sh
+++ clipboard-to-pdf/run-script.sh
@@ -2,7 +2,7 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Markdown to PDF
+# @raycast.title Clipboard to PDF
 # @raycast.mode compact
 
 # Optional parameters:
@@ -10,7 +10,7 @@
 # @raycast.argument1 { "type": "text", "placeholder": "Output filename" }
 
 # Documentation:
-# @raycast.description Converts markdown content from the clipboard to a PDF output in the current active Finder window or /output folder.
+# @raycast.description Converts clipboard content (markdown or plain text) to a PDF output in the current active Finder window.
 # @raycast.author Nick Trombley
 # @raycast.authorURL https://raycast.com/Aias
 
@@ -30,4 +30,4 @@ else
 fi
 
 # Run the Node.js script
-"$NODE_PATH" "$SCRIPT_DIR/index.js" "$1"
+"$NODE_PATH" "$SCRIPT_DIR/main.js" "$1"
